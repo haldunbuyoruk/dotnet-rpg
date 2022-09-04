7d7f8ba1b5ffc7d40060e82261f79703499811b6
Haldun Buyorük <haldunbuyoruk@gmail.com>
2022-09-04 03:14:47 +0300

    Service Layer added

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnet_rpg.Services.CharacterService;
 using Microsoft.AspNetCore.Mvc;
 
 
@@ -11,26 +12,34 @@ namespace dotnet_rpg.Controllers
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static List<Character> characters = new List<Character>
-        {
-            new Character(),
-            new Character {Id = 1, Name = "Sam"}
-        };
+        private readonly ICharacterService characterService;
 
-       
+        public CharacterController(ICharacterService
+            characterService)
+        {
+            this.characterService = characterService;
+        }
+
         [HttpGet("GetAll")]
         public ActionResult<List<Character>> Get()
         {
-            return Ok(characters);
+            return Ok(this.characterService.GetAllCharacters());
         }
 
         [HttpGet("{id}")]
         public ActionResult<Character> GetSingle(int id)
         {
-            return Ok(characters.FirstOrDefault(c => c.Id == id));
+            return Ok(this.characterService.GetCharacterById(id));
+        }
+
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+        {
+            
+            return Ok(this.characterService.AddCharacter(newCharacter));
         }
     }
 
-    
+
 }
 
---
+++ Services/CharacterService/ICharacterService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace dotnet_rpg.Services.CharacterService
+{
+    public interface ICharacterService
+    {
+        List<Character> GetAllCharacters();
+
+        Character GetCharacterById(int id);
+
+        List<Character> AddCharacter(Character newCharacter);
+    }
+}
+
