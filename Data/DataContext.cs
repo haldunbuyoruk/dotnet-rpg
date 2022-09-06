fce1bf6c6a2f0ce685e95c47187826de4aa79dcf
Haldun Buyorük <haldunbuyoruk@gmail.com>
2022-09-06 22:33:08 +0300

    example for seed data

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -12,7 +12,31 @@ namespace dotnet_rpg.Data
 
         public DbSet<Character> Characters { get; set; }
 
-
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Character>().HasData(
+                new Character
+                {
+                    Id = 1,
+                    Name = "Frodo",
+                    Class = RpgClass.Knight,
+                    HitPoints = 100,
+                    Strength = 15,
+                    Defense = 10,
+                    Intelligence = 10
+                },
+                new Character
+                {
+                    Id = 2,
+                    Name = "Raistlin",
+                    Class = RpgClass.Mage,
+                    HitPoints = 100,
+                    Strength = 5,
+                    Defense = 5,
+                    Intelligence = 20
+                }
+            );
+        }
     }
 }
 
---
+++ Migrations/20220906192922_DataSeedExample.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet_rpg.Migrations
+{
+    public partial class DataSeedExample : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Characters",
+                columns: new[] { "Id", "Class", "Defense", "HitPoints", "Intelligence", "Name", "Strength" },
+                values: new object[] { 1, 1, 10, 100, 10, "Frodo", 15 });
+
+            migrationBuilder.InsertData(
+                table: "Characters",
+                columns: new[] { "Id", "Class", "Defense", "HitPoints", "Intelligence", "Name", "Strength" },
+                values: new object[] { 2, 2, 5, 100, 20, "Raistlin", 5 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Characters",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
